3015ad5156e76869a1253c60effa2def5dd185ca
Madeline MORISSET <madeline.morisset@outlook>
2023-08-29 11:32:51 +0200

    dev - finishing of desktop version of the homepage

---
+++ webroot/assets/includes/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" media="screen" title="no title"
+      charset="utf-8" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="./webroot/app.css" />
+    <!-- Nav tab's icon -->
+    <link rel="shortcut icon" href="webroot/assets/img/logoV1.ico" type="image/x-icon">
+    <!-- Kit Font Awesome -->
+    <script src="https://kit.fontawesome.com/23c1a897ea.js" crossorigin="anonymous"></script>
+    <!-- JS, React et Babel -->
+    <script src="app.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/react@17/umd/react.development.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/react-dom@17/umd/react-dom.development.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/babel-standalone@6.26.0/babel.min.js"></script>
+    <script type="text/babel" src="src/index.js"></script> -->
+
+    <title>Portfolio - MMorisset</title>
+    <meta name="author" content="MMORISSET"/>
+    <meta name="description" content="Portfolio de Madeline MORISSET, apprenante en Développement Web et Web Mobile"/>
+  </head>
+
+  <body class="siteBody">
--- webroot/assets/includes/siteHeader.php
+++ webroot/assets/includes/siteHeader.php
@@ -6,15 +6,20 @@
         <section class="nav-menuBurger" onClick="navHidden()">
             <section class="nav-menuBurger--button button40px squareButton"><i class="fa-solid fa-bars"></i></section>
             <section id="navHidden" class="nav-menuBurger--menu visibilityNav">
-                <section class="nav-menuBurger--links titleBox">
-                    <a href="index.php" class="nav-menuBurger--link nav-title--semiBold titleHeader">Accueil</a>
-                    <hr>
+                <section class="nav-menuBurger--links nav-menuBurger--separationLine ">
+                    <a href="index.php" class="nav-menuBurger--link nav-title--semiBold">Accueil</a>
                 </section>
-
-                <section class="nav-menuBurger--links titleBox">
-                    <a href="projects.php" class="nav-menuBurger--link nav-title--semiBold titleHeader">Projets</a>
-                    <a href="=trainingExperience.php" class="nav-menuBurger--link nav-title--semiBold titleHeader">Formations / Expériences</a>
-                    <a href="intershipReport.php" class="nav-menuBurger--link nav-title--semiBold titleHeader">Rapport de stage</a>
+                <section class="nav-menuBurger--links nav-menuBurger--separationLine">
+                    <a href="" class="nav-menuBurger--link nav-title--semiBold">Projets</a>
+                    <!-- href="projects.php" -->
+                </section>
+                <section class="nav-menuBurger--links nav-menuBurger--separationLine">
+                    <a href="" class="nav-menuBurger--link nav-title--semiBold">Formations - Expériences</a>
+                    <!-- href="trainingExperience.php" -->
+                </section>
+                <section class="nav-menuBurger--links">
+                    <a href="" class="nav-menuBurger--link nav-title--semiBold">Rapport de stage</a>
+                    <!-- href="intershipReport.php" -->
                 </section>
             </section>
         </section> 
